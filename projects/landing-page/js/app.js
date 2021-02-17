07788327322a8d913dc8467d16f0ae59e7613aa7
Hugo P. Costa <hugopacosta@yahoo.com.br>
2021-02-17 16:41:58 -0300

    feat:Include dynamically generated navigation menu

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,41 +17,86 @@
  * Define Global Variables
  * 
 */
-
+let sectionList = document.querySelectorAll('section');
+const mainElement = document.querySelector('main');
+const observerConfig = { attributes: false, childList: true, subtree: false };
 
 /**
  * End Global Variables
- * Start Helper Functions
+ * Start Main Functions
  * 
 */
 
+const callback = function(mutationsList, observer) {
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            console.log('A child node has been added or removed.');
+            fillUnorderedList();
+        }
+    }
+};
 
+const observer = new MutationObserver(callback);
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+observer.observe(mainElement, observerConfig);
 
-// build the nav
+function elementClosestToTheTop(){
+    let smallestDistance = null;
+    let closestSection;
+    for(const sect of sectionList){
+        let distance = sect.getBoundingClientRect().top;
+        if(smallestDistance == null){
+            smallestDistance = distance;
+            closestSection = sect;
+        } 
+        else if(Math.abs(distance) < Math.abs(smallestDistance)){
+            smallestDistance = distance;
+            closestSection = sect;
+        } 
+    }
+    return closestSection;
+}
 
+function addSection(){
+    const section = document.createElement('section');
+    const sectionNumber = sectionList.length + 1;
+    section.id = `section${sectionNumber}`;
+    section.setAttribute('data-nav', `Section ${sectionNumber}`);
+    mainElement.appendChild(section);
+}
 
-// Add class 'active' to section when near top of viewport
+function removeSection(){
+    mainElement.removeChild(mainElement.lastElementChild);
+}
 
+function fillUnorderedList(){
+    sectionList = document.querySelectorAll('section');
+    const navList = document.querySelector('#navbar__list');
+    navList.innerHTML = '';
+    for(const sect of sectionList){
+        let listItem = document.createElement('li');
+        listItem.textContent = sect.dataset.nav;
+        listItem.className = 'menu__link';
+        navList.appendChild(listItem);
+        listItem.addEventListener('click', function(){
+            sect.scrollIntoView({behavior: 'smooth'});
+        });
+    }
+}
 
-// Scroll to anchor ID using scrollTO event
-
+function toggleActiveSection(){
+    for(const sect of sectionList){
+        sect.classList.remove('active-section');
+    }
+    const activeSection = elementClosestToTheTop();
+    activeSection.classList.add('active-section');
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
+document.addEventListener('scroll', toggleActiveSection);
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+document.addEventListener('DOMContentLoaded', fillUnorderedList);
